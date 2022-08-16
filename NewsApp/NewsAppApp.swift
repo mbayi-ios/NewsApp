33a233610933153bb8ca2284342218a0c417c252
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-08-17 00:19:55 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Amby on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
